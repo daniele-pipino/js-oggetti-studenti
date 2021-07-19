34a07200712a9e94979f4d239325dca1de2facc8
daniele-pipino <Danielemacpipino@icloud.com>
2021-07-19 16:30:04 +0200

    es-2 Creazione array di studenti

---
+++ js/students-2.js
@@ -0,0 +1,33 @@
+/*ES 2
+Creare un array di oggetti di studenti.
+Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età. */
+
+//* --------------------------------SUDDIVIDIONE DELLA TRACCIA------------------------------
+
+//* 1- Creare un array di oggetti studenti
+//* 2- Creare un ciclo for in per girare nell'array di oggetti
+//* 3- Creare 3 prompt che permettano all'utente di inserire un nuovo oggetto studente nella lista con nome cognome ed età
+
+//* --------------------------------SVOLGIMENTO TRACCIA---------------------------------------
+
+// Creazione array di studenti 
+
+var students = [
+    // vari studenti
+    {
+        nome: 'Gianni',
+        cognome: 'Palaggiano'
+    },
+    {
+        nome: 'Daniele',
+        Cognome: 'Pipino',
+    },
+    {
+        nome: 'Massimo',
+        cognome: 'Saracino'
+    }
+
+]
+
+// 
