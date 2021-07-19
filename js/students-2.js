b7e8c1d6b9804942aa07c23db9f55eab568818a6
daniele-pipino <Danielemacpipino@icloud.com>
2021-07-19 16:46:55 +0200

    Inserimento nuovo utente

--- js/students-2.js
+++ js/students-2.js
@@ -8,9 +8,13 @@ Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo
 //* 1- Creare un array di oggetti studenti
 //* 2- Creare un ciclo for in per girare nell'array di oggetti
 //* 3- Creare 3 prompt che permettano all'utente di inserire un nuovo oggetto studente nella lista con nome cognome ed età
-
+//* 4- Stampare tutti i dati degli utenti in pagina
 //* --------------------------------SVOLGIMENTO TRACCIA---------------------------------------
 
+// recupero elementi html
+var studentsName = document.getElementById('nome');
+var studentsSurname = document.getElementById('cognome');
+var studentsAge = document.getElementById('eta');
 // Creazione array di studenti 
 
 var students = [
@@ -21,7 +25,7 @@ var students = [
     },
     {
         nome: 'Daniele',
-        Cognome: 'Pipino',
+        cognome: 'Pipino',
     },
     {
         nome: 'Massimo',
@@ -30,4 +34,23 @@ var students = [
 
 ]
 
-// 
+// Creazione dei prompt
+
+var studentName = prompt('Inserisci il nome del nuovo studente');
+//todo Validazione da fare
+var studentSurname = prompt('Inserisci il cognome del nuovo studente');
+//todo Validazione da fare
+var studentAge = parseInt(prompt("Inserisci l'eta del nuovo studente"));
+//todo Validazione da fare
+
+// Dati nuovo studente
+var newStudent = {
+
+    nome: studentName,
+    cognome: studentSurname,
+    age: studentAge,
+
+}
+
+// Inserimento nuovo utente nella lista di quelli gia esistenti
+students.push(newStudent);
