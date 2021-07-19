47d730a6acf24e52b6494b7fa5317030e143214d
daniele-pipino <Danielemacpipino@icloud.com>
2021-07-19 14:38:09 +0200

    initial commit + students object

---
+++ js/students-1.js
@@ -0,0 +1,12 @@
+/*ES 1
+Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+Stampare a schermo (in pagina) attraverso un ciclo for-in tutte le proprietà dell'oggetto. */
+
+
+//* Creazione oggetto students
+
+var students = {
+    name: 'Daniele',
+    surname: 'Pipino',
+    age: 21,
+};
